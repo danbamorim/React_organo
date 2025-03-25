c21f019e3fb6749c06e73a935717fef88f3bf6eb
Danilo Amorim <danbamorim@gmail.com>
2025-03-24 22:07:40 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+
+function Banner() {
+    return (
+        <header className="banner"> 
+            <img src="/imagens/banner.png" alt="o banner principal da pagina do oragano" />
+        </header>
+    )
+
+}
+
+export default Banner
